5575fc8c344ddb1eb1eb5b43816787e7e44fb418
Barnabás Birmacher <birmacher@gmail.com>
2017-08-31 18:46:17 +0200

    Initial Commit

---
+++ uitest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uitest
+//
+//  Created by Barnabás Birmacher on 8/31/17.
+//  Copyright © 2017 Barnabás Birmacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
